df5fa1af3afff293b832448c28bd8459da442db0
brunastasi <60503962+brunastasi@users.noreply.github.com>
2020-03-09 19:03:13 +0100

    09/03 19H03

--- Controllers/AdherentsController.cs
+++ Controllers/AdherentsController.cs
@@ -115,65 +115,6 @@ namespace Assortie.Controllers
             return View(adherent);
         }
 
-        // GET: Adherents/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Adherent adherent = db.Adherents.Find(id);
-            if (adherent == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.IdAssociation = new SelectList(db.Associations, "IdAssociation", "Nom", adherent.IdAssociation);
-            return View(adherent);
-        }
-
-        // POST: Adherents/Edit/5
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "IdAdherent,IdAssociation,Matricule,Nom,Prenom,Email,Telephone,Cotisation,Login,Password,Responsable,Solde")] Adherent adherent)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(adherent).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.IdAssociation = new SelectList(db.Associations, "IdAssociation", "Nom", adherent.IdAssociation);
-            return View(adherent);
-        }
-
-        // GET: Adherents/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Adherent adherent = db.Adherents.Find(id);
-            if (adherent == null)
-            {
-                return HttpNotFound();
-            }
-            return View(adherent);
-        }
-
-        // POST: Adherents/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Adherent adherent = db.Adherents.Find(id);
-            db.Adherents.Remove(adherent);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
